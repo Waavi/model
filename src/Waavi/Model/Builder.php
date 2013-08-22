3ad32cec7f5292faaa92c512d3300a555400cf82
William <william@amovens.com>
2013-08-23 01:48:51 +0200

    Allow for nested whereRelated and orWhereRelated
    Signed-off-by: William <william@amovens.com>

--- src/Waavi/Model/Builder.php
+++ src/Waavi/Model/Builder.php
@@ -1,8 +1,10 @@
 <?php namespace Waavi\Model;
 
+use Closure;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
-class Builder extends \Illuminate\Database\Eloquent\Builder {
+class Builder extends EloquentBuilder {
 
 	/**
 	 * Filters the result set by querying related models. This is a very limited method, as it only works when the
@@ -19,8 +21,13 @@ class Builder extends \Illuminate\Database\Eloquent\Builder {
 	 * @param  string  $boolean
 	 * @return \Illuminate\Database\Query\Builder|static
 	 */
-	public function whereRelated($model, $column, $operator = null, $value = null, $boolean = 'and')
+	public function whereRelated($model, $column = null, $operator = null, $value = null, $boolean = 'and')
 	{
+		if (is_callable($model))
+		{
+			return $this->whereRelatedNested($model, $boolean);
+		}
+
 		$relation = $this->getRelation($model);
 		$relationType = str_replace('Illuminate\\Database\\Eloquent\\Relations\\', '', get_class($relation));
 		switch($relationType) {
@@ -42,9 +49,41 @@ class Builder extends \Illuminate\Database\Eloquent\Builder {
 		}
 	}
 
-	public function orWhereRelated($model, $column, $operator = null, $value = null)
+	public function orWhereRelated($model, $column = null, $operator = null, $value = null)
 	{
 		return $this->whereRelated($model, $column, $operator, $value, 'or');
 	}
 
+	/**
+	 * Add a nested where statement to the query.
+	 *
+	 * @param  \Closure $callback
+	 * @param  string   $boolean
+	 * @return \Illuminate\Database\Query\Builder|static
+	 */
+	public function whereRelatedNested(Closure $callback, $boolean = 'and')
+	{
+		// To handle nested queries we'll actually create a brand new query instance
+		// and pass it off to the Closure that we have. The Closure can simply do
+		// do whatever it wants to a query then we will store it for compiling.
+		$type = 'Nested';
+
+		$builder = new Builder($this->query->newQuery());
+		$builder->setModel($this->model);
+
+		call_user_func($callback, $builder);
+
+		// Once we have let the Closure do its things, we can gather the bindings on
+		// the nested query builder and merge them into these bindings since they
+		// need to get extracted out of the children and assigned to the array.
+		$query = $builder->getQuery();
+		if (count($query->wheres))
+		{
+			$this->getQuery()->wheres[] = compact('type', 'query', 'boolean');
+
+			$this->getQuery()->mergeBindings($query);
+		}
+
+		return $this;
+	}
 }
